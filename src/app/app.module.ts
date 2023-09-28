e2f981b9204d0bcc45de28b6d271d47e451c163b
Franco Chiavassa <fraancotiago@hotmail.com>
2023-09-28 20:23:28 -0300

    Segundo commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/main-wrapper/main-wrapper.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-wrapper',
+  template: `
+    <main>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+    <h1> Alumnos </h1>
+    <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione rerum magnam necessitatibus odio! Ratione doloremque sequi, ipsam eos quae consequatur exercitationem ea dolore cumque consectetur laudantium ut, numquam quaerat quidem?
+    </p>
+</main>
+  `,
+  styleUrls: ['./main-wrapper.component.css']
+})
+export class MainWrapperComponent {
+
+}
